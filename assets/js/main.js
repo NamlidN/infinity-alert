d721f0e07ef206f8c85bb10d75f9f48d1712628a
NamlidN <dilman.n999@gmail.com>
2022-10-10 15:38:13 +0200

    "asdff"

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,33 +3,34 @@ for (i = 0; i < 5; i++) {
     alert('Nervt Das???')
 }
 alert('HaLlO?!?!')
-var i;
-for (i = 0; i < 5; i++) {
+var a;
+for (a = 0; a < 5; a++) {
     alert('Nervt Das???')
 }
 alert('HaLlO?!?!')
-var i;
-for (i = 0; i < 5; i++) {
+var b;
+for (b = 0; b < 5; b++) {
     alert('Nervt Das???')
 }
 alert('HaLlO?!?!')
-var i;
-for (i = 0; i < 5; i++) {
+var c;
+for (c = 0; c < 5; c++) {
+    alert('Nervt Das???')
+}
+
+alert('HaLlO?!?!')
+var d;
+for (d = 0; d < 5; d++) {
+    prompt('Nervt Das???')
+}
+alert('HaLlO?!?!')
+var e;
+for (e = 0; e < 5; e++) {
     alert('Nervt Das???')
 }
 alert('HaLlO?!?!')
-var i;
-for (i = 0; i < 5; i++) {
-    alert('Nervt Das???')
-}
-alert('HaLlO?!?!')
-var i;
-for (i = 0; i < 5; i++) {
-    alert('Nervt Das???')
-}
-alert('HaLlO?!?!')
-var i;
-for (i = 0; i < 5; i++) {
+var y;
+for (y = 0; y < 5; y++) {
     prompt('Nervt Das???')
 }
 alert('HaLlO?!?!')
